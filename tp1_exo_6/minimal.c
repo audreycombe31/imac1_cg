b9171592a4fdb30b39f5f9b3751cd15b37cfcb9f
audreycombe <audrey.combe31@gmail.com>
2018-02-11 16:21:08 +0100

    fin exo 5 + début exo 6

--- tp1_exo_6/minimal.c
+++ tp1_exo_6/minimal.c
@@ -8,11 +8,20 @@
 static unsigned int WINDOW_WIDTH = 400;
 static unsigned int WINDOW_HEIGHT = 400;
 
+/*Type d'objet à dessiner*/
+static unsigned char P = GL_POINTS;
+static unsigned char L = GL_LINES;
+static unsigned char T = GL_TRIANGLES;
+
 /* Nombre de bits par pixel de la fenêtre */
 static const unsigned int BIT_PER_PIXEL = 32;
 
 /* Nombre minimal de millisecondes separant le rendu de deux images */
 static const Uint32 FRAMERATE_MILLISECONDS = 1000 / 60;
+
+/*variables globales*/
+float posX=1.0;
+float posY=1.0;
 
 void resize(int width, int height){
 	WINDOW_HEIGHT= height;
@@ -20,20 +29,52 @@ void resize(int width, int height){
 	SDL_SetVideoMode(WINDOW_WIDTH, WINDOW_HEIGHT, BIT_PER_PIXEL, SDL_OPENGL | SDL_RESIZABLE);
 }
 
-/*Exercice 5
- *
- */
-void projection(){
-    glViewport(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
+
+void dessinerForme(char forme){
+	float x=1.0;
+	float y=1.0;
+	
+	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 0);
+	glViewport(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
     gluOrtho2D(-1., 1., -1., 1.);
 
-    /*Dessinons des points*/
-	glPointSize(10);
-   	glClear(GL_COLOR_BUFFER_BIT);
-    glBegin(GL_POINTS);
-        glVertex2f(-1 + 2. * 200 / WINDOW_WIDTH, -(-1 + 2. *200 / WINDOW_HEIGHT));
+	 /* Boucle traitant les evenements */
+     SDL_Event e;
+     while(SDL_PollEvent(&e)) {
+
+		
+		switch(e.type) {
+
+			/* Clic souris */
+			case SDL_MOUSEBUTTONDOWN:
+				printf("clic en (%d, %d)\n", e.button.x, e.button.y);
+				x=e.button.x;
+				y=e.button.y;
+				/*Changer couleur fond
+				glClear(GL_COLOR_BUFFER_BIT);
+				glClearColor((x/WINDOW_WIDTH), (y/WINDOW_HEIGHT), 0, 1);
+				*/
+				break;
+		}
+	 }
+    
+	glPointSize(5);
+
+    glBegin(forme);
+		glColor3f(x/WINDOW_WIDTH,0,y/WINDOW_HEIGHT);
+		if(forme == P)
+        	glVertex2f(-1 + 2. * posX / WINDOW_WIDTH, -(-1 + 2. *posY / WINDOW_HEIGHT));
+		if(forme == L){
+        	glVertex2f(-1 + 2. * x / WINDOW_WIDTH, -(-1 + 2. *y / WINDOW_HEIGHT));
+        	glVertex2f(-1 + 2. * x+20 / WINDOW_WIDTH, -(-1 + 2. *y / WINDOW_HEIGHT));
+		}
+		if(forme == T){
+        	glVertex2f(-1 + 2. * x / WINDOW_WIDTH, -(-1 + 2. *y / WINDOW_HEIGHT));
+        	glVertex2f(-1 + 2. * x / WINDOW_WIDTH, -(-1 + 2. *y / WINDOW_HEIGHT));
+        	glVertex2f(-1 + 2. * x / WINDOW_WIDTH, -(-1 + 2. *y / WINDOW_HEIGHT));
+		}
     glEnd();
 }
 
@@ -49,13 +90,36 @@ void dessinerPoint(float x, float y){
 
     /*Dessinons des points*/
 	glPointSize(5);
-   	//glClear(GL_COLOR_BUFFER_BIT);
-    glBegin(GL_POINTS);
+
+    glBegin(P);
 		glColor3f(x/WINDOW_WIDTH,0,y/WINDOW_HEIGHT);
-        glVertex2f(-1 + 2. * x / WINDOW_WIDTH, -(-1 + 2. *y / WINDOW_HEIGHT));
+        glVertex2f(-1 + 2. * posX / WINDOW_WIDTH, -(-1 + 2. *posY / WINDOW_HEIGHT));
     glEnd();
 }
+
+/*Role : dessiner une ligne
+ *Paramètres : x et y : coordonnées du point
+*/
+void dessinerLigne(float x, float y){
+	char forme = L;
+	SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 0);
+	glViewport(0, 0, WINDOW_WIDTH, WINDOW_HEIGHT);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluOrtho2D(-1., 1., -1., 1.);
+
+    /*Dessinons des lignes*/
 	
+
+    glBegin(forme);
+		glColor3f(x/WINDOW_WIDTH,0,y/WINDOW_HEIGHT);
+        glVertex2f(-1 + 2. * x / WINDOW_WIDTH, -(-1 + 2. *y / WINDOW_HEIGHT));
+	 	glVertex2f(-1 + 2. * (x+10) / WINDOW_WIDTH, -(-1 + 2. *y / WINDOW_HEIGHT));
+		
+				
+    glEnd();
+}	
+
 
 int main(int argc, char** argv) {
 
@@ -110,33 +174,51 @@ int main(int argc, char** argv) {
             switch(e.type) {
 
                 /* Clic souris */
-                case SDL_MOUSEBUTTONUP:
+               /* case SDL_MOUSEBUTTONDOWN:
                     printf("clic en (%d, %d)\n", e.button.x, e.button.y);
                     x=e.button.x;
-                    y=e.button.y;
+                    y=e.button.y;*/
 					/*Changer couleur fond
                     glClear(GL_COLOR_BUFFER_BIT);
                     glClearColor((x/WINDOW_WIDTH), (y/WINDOW_HEIGHT), 0, 1);
 					*/
-					dessinerPoint(x, y);
-                    break;
-
-                /* Touche clavier */
-                case SDL_KEYDOWN:
-                    printf("touche pressée (code = %d)\n", e.key.keysym.sym);
-                    break;
-
-                case SDL_MOUSEMOTION:
-                    x=e.motion.x;
-                    y=e.motion.y;
-                    /* Changer couleur fond
-					glClear(GL_COLOR_BUFFER_BIT);
+					/*dessinerForme(L);
+                    break;*/
+					
+				/*case SDL_MOUSEBUTTONUP:
+                    printf("clic en (%d, %d)\n", e.button.x, e.button.y);
+                    x=e.button.x;
+                    y=e.button.y;*/
+					/*Changer couleur fond
+                    glClear(GL_COLOR_BUFFER_BIT);
                     glClearColor((x/WINDOW_WIDTH), (y/WINDOW_HEIGHT), 0, 1);
 					*/
-                    break;
+					/*dessinerLigne(x, y);
+                    break;*/
+
+                /* Touche clavier 
+                case SDL_KEYDOWN:
+                    printf("touche pressée (code = %d)\n", e.key.keysym.sym);
+                    break;*/
+					
+				case SDL_KEYDOWN:
+					if(e.key.keysym.sym == 112){
+						if(SDL_MOUSEBUTTONDOWN){
+                    		printf("clic en (%d, %d)\n", e.button.x, e.button.y);
+                    		posX=e.button.x;
+                    		posY=e.button.y;
+						}
+						dessinerForme(P);
+					}
+					if(e.key.keysym.sym == 108)
+						dessinerForme(L);
+					//if(e.key.keysym.sym == 116)
+					//	dessinerTriangle(x,y);
+							
+					break;
 
                 case SDL_VIDEORESIZE:
-                    projection();
+                    //projection();
 					resize(e.resize.w, e.resize.h);
                     break;
 
